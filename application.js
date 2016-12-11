f747488dfa1f16d22e4be8fa1eeb61d2b17e2ddd
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-11 14:42:58 -0800

    add react code to test if linked properly

--- application.js
+++ application.js
@@ -1,15 +1,27 @@
-$(document).ready(function() {
-  // Initially disable the button
-  $('button').prop('disabled', true);
-  // When the value of the text area changes
-  $('textarea').on('input', function() {
-      // If there's a least one character in the textarea
-      if ($(this).val().length > 0) {
-        // Enable the button
-        $('button').prop('disabled', false);
-      } else {
-        // Else, disable the button
-        $('button').prop('disabled', true);
-      }
-  });
+var TweetBox = React.createClass({
+  render: function() {
+    return (
+        <div>
+          Hello World!
+        </div>
+    );
+  }
 });
+
+
+// JS without react
+// $(document).ready(function() {
+//   // Initially disable the button
+//   $('button').prop('disabled', true);
+//   // When the value of the text area changes
+//   $('textarea').on('input', function() {
+//       // If there's a least one character in the textarea
+//       if ($(this).val().length > 0) {
+//         // Enable the button
+//         $('button').prop('disabled', false);
+//       } else {
+//         // Else, disable the button
+//         $('button').prop('disabled', true);
+//       }
+//   });
+// });
