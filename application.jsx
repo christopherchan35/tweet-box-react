68e7860e6122f700d5b11cf843728a7d61851faa
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-11 16:34:34 -0800

    testing random phrase generator

---
+++ application.jsx
@@ -0,0 +1,14 @@
+var TweetBox = React.createClass({
+  render: function() {
+    return (
+        <div>
+          Hello World!
+        </div>
+    );
+  }
+});
+//
+ReactDOM.render(
+  <TweetBox />,
+  document.getElementById('container');
+);
