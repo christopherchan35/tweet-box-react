ee506ec222ac1612357b52a591e3f0895ef55279
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-12 12:22:33 -0800

    highlights text that exceeds the 140 character limit

--- tweet.jsx
+++ tweet.jsx
@@ -20,9 +20,13 @@ var TweetBox = React.createClass({
   },
   overflowAlert: function() {
     if (this.remainingCharacters() < 0) {
+      var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+      var overflowText = this.state.text.substring(140);
       return (
         <div className="alert alert-warning">
           <strong>Oops! Too Long:</strong>
+          &nbsp;...{beforeOverflowText}
+          <strong className="bg-danger">{overflowText}</strong>
         </div>
       );
     } else {
