0413d844072e76652f8ef41ceda39d6ef888bbb4
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-11 16:47:20 -0800

    change message by click of button

--- application.jsx
+++ application.jsx
@@ -1,14 +1,14 @@
-var TweetBox = React.createClass({
-  render: function() {
-    return (
-        <div>
-          Hello World!
-        </div>
-    );
-  }
-});
-//
-ReactDOM.render(
-  <TweetBox />,
-  document.getElementById('container');
-);
+// var TweetBox = React.createClass({
+//   render: function() {
+//     return (
+//         <div>
+//           Hello World!
+//         </div>
+//     );
+//   }
+// });
+// //
+// ReactDOM.render(
+//   <TweetBox />,
+//   document.getElementById('container');
+// );
