b58a24eb03e8293ab51a347d0e93a754212550c6
Christopher Chan <chris@Christophers-MacBook-Pro-2.local>
2016-12-12 08:57:34 -0800

    implement handleChange function on textarea

--- tweet.jsx
+++ tweet.jsx
@@ -1,11 +1,12 @@
 var TweetBox = React.createClass({
   handleChange: function(event){
-
+    console.log(event.target.value);
   },
   render: function() {
     return (
       <div className="well clearfix">
-        <textarea className="form-control"></textarea>
+        <textarea className="form-control"
+                  onChange={this.handleChange}></textarea>
         <br/>
         <button className="btn btn-primary pull-right" disabled>Tweet</button>
       </div>
